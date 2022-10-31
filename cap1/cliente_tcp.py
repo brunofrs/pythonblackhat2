607c8d5b16a66f19ebc4e4ed1cb9d9ba0dad441c
brunofrs <bruninhofrs@gmail.com>
2022-10-31 18:07:36 -0300

    commit

---
+++ cap1/cliente_tcp.py
@@ -0,0 +1,20 @@
+import socket
+
+
+target_host= "www.google.com"
+target_port= 80
+
+#1 - cria um cliente do objeto socket
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#2 - Conecta no cliente
+client.connect((target_host,target_port))
+
+#3 - envia os dados
+client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+
+#4 - Recebendo os dados da mensagem
+response = client.recv(4096)
+
+print(response.decode())
+client.close()
